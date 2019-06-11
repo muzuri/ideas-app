4d3c6a7839cc0bbaa829a3ee7f27e73a087e1ad7
muzuri <muzuri31@gmail.com>
2019-06-11 09:46:18 +0200

    services added in app module in providers array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import { HttpClientModule} from '@angular/common/http';
-
-
+import {ApiService} from '@app/services/api.service';
+import {AuthService} from '@app/services/auth.service';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 
@@ -21,7 +21,7 @@ import { AppComponent } from '@app/app.component';
 
 
   ],
-  providers: [],
+  providers: [ApiService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { AuthDTO } from '../models/user';
+import { AuthDTO, User } from '../models/user';
 import { AuthType } from '@app/models/auth';
+import { Observable } from 'rxjs';
 
 
 
@@ -10,11 +11,25 @@ import { AuthType } from '@app/models/auth';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl: string = environment.api_server;
+  private apiUrl: string = environment.api_server + '/api';
 
   constructor(private http: HttpClient) { }
 
-  get token() {
+  private auth(authtype: AuthType, data: any ): Observable<any> {
+    return this.http.post(`${this.apiUrl}/${authtype}`, data);
+
+  }
+
+  login(data: AuthDTO): Observable<User> {
+    return this.auth('login', data);
+
+  }
+
+  register(data: AuthDTO): Observable<User> {
+    return this.auth('register', data);
+  }
+
+  get token(): string {
     return localStorage.get('idea-token');
   }
   set token(val: string) {
@@ -22,26 +37,12 @@ export class AuthService {
       localStorage.setItem('idea-token', val);
 
     } else {
-
+ localStorage.clear();
     }
   }
-  private auth(authtype: AuthType, data: any ) {
-    return this.http.post(`${this.apiUrl}/${authtype}`, data);
-
-  }
-
-  login() {
-
-  }
-
-  public register(data: AuthDTO) {
-    return this.http.post(`${this.apiUrl}/register`, data);
-  }
-  public async whoami(username: string) {
-    return await this.http.get(`${this.apiUrl}/auth/whomai/${username}`);
-
-
-  }
+whoami(){
+ return this.http.get(`${this.apiUrl}/whoami`, {headers: {authorization: `Bearer ${this.token}`}});
+}
 
 }
   // @Post('api/register')
